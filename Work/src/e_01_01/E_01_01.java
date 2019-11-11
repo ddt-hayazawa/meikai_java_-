ec8a7baf9b42c582a804bd4ec277a1f9b9a3f0cb
hayazawa <ddt.khayazawa@gmail.com>
2019-11-11 19:55:58 +0900

    2019/11/11_演習01-01_1回目_進捗率100%_セミコロンなしの挙動を確認

---
+++ Work/src/e_01_01/E_01_01.java
@@ -0,0 +1,11 @@
+package e_01_01;
+
+public class E_01_01 {
+	public static void main(String[] args){
+		// セミコロンが不足している状態だとどうなるのか確認
+		System.out.println("初めてのJavaプログラム")
+
+		// 構文エラーで実行できないことを確認
+	}
+
+}

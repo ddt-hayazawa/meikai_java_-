f228d74dd985b47558e510854aa7a2a283624ede
hayazawa <ddt.khayazawa@gmail.com>
2019-11-13 20:07:34 +0900

    2019/11/13_演習2-03_1回目_進捗率100％_キーボードから読み込んだ整数値をそのまま反復して表示する

---
+++ Work/src/e_02_02/E_02_03.java
@@ -0,0 +1,18 @@
+package e_02_02;
+import java.util.Scanner;
+
+public class E_02_03 {
+
+	// キーボードから読み込んだ整数値をそのまま反復して表示する
+	public static void main(String[] args){
+
+		// scannerクラスのインスタンスを作成
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("整数値："); // 「整数値：」を表示
+		int a = stdIn.nextInt(); 	  // aに整数値を読み込む
+
+		System.out.println(a + "と入力しましたね。"); // 入力された整数値と「と入力しましたね。」を結合し表示
+
+	}
+}

3b77dcb67375b7b1f0366d6674db772ff8b16fce
hayazawa <ddt.khayazawa@gmail.com>
2019-12-11 19:13:02 +0900

    2019/12/11_演習03-01_1回目_整数値を読み込んでその絶対値を求めて表示する

---
+++ Work/src/e_03_01/E_03_01.java
@@ -0,0 +1,22 @@
+package e_03_01;
+import java.util.Scanner;
+
+public class E_03_01 {
+    //整数値を読み込んでその絶対値を求めて表示する
+    public static void main(String[] args){
+        // Scannerクラスのインスタンスを生成
+        Scanner stdIn = new Scanner(System.in);
+
+        // 整数値の入力を促す
+        System.out.print("整数値：");
+        // 入力値を変数に代入
+        int intValue = stdIn.nextInt();
+
+        if(intValue < 0)
+            // 入力値が0未満(マイナス)であればマイナスをかけて絶対値を算出
+            intValue = intValue * -1;
+        // 算出した絶対値を表示
+        System.out.println("その絶対値は" + intValue + "です。" );
+    }
+
+}

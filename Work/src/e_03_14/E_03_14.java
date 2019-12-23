d2a6217120c221a390a879f3fb1cc9a566b40145
hayazawa <ddt.khayazawa@gmail.com>
2019-12-23 21:30:41 +0900

    2019/12/23_演習03-14_1回目_進捗率50％_2つの実数値を読み込み小さい値・大きい値両方を表示する

---
+++ Work/src/e_03_14/E_03_14.java
@@ -0,0 +1,22 @@
+package e_03_14;
+import java.util.Scanner;
+
+public class E_03_14 {
+	// 2つの実数値を読み込み小さい値・大きい値両方を表示する
+	public static void main(String[] args){
+		// Scannerクラスのインスタンスを生成
+		Scanner stdIn = new Scanner(System.in);
+
+		// 実数値aの入力を促す
+		System.out.print("整数a：");
+		// 実数値aの入力値を変数に代入
+		int intValueA = stdIn.nextInt();
+
+		// 実数値bの入力を促す
+		System.out.print("整数b：");
+		// 実数値bの入力値を変数に代入
+		int intValueB = stdIn.nextInt();
+
+
+	}
+}

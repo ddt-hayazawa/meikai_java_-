1086aaf012f4b1113ddd348af04f66341f851cf8
hayazawa <ddt.khayazawa@gmail.com>
2019-12-23 19:50:19 +0900

    2019/12/23_演習03-09_1回目_進捗率100％_二つの実数値を読み込んで大きいほうの値を表示する

---
+++ Work/src/e_03_09/E_03_09.java
@@ -0,0 +1,27 @@
+package e_03_09;
+
+import java.util.Scanner;
+
+public class E_03_09 {
+	// 二つの実数値を読み込んで大きいほうの値を表示する
+	public static void main(String[] args){
+		// Scannerクラスのインスタンスを生成
+		Scanner stdIn = new Scanner(System.in);
+
+		// 実数値aの入力を促す
+		System.out.print("整数a：");
+		// 実数値aの入力値を変数に代入
+		int intValueA = stdIn.nextInt();
+
+		// 実数値bの入力を促す
+		System.out.print("整数b：");
+		// 実数値bの入力値を変数に代入
+		int intValueB = stdIn.nextInt();
+
+		// 表示用の数値を入れる変数を宣言
+		int maxValue;
+
+		// 参考演算子で一気に計算
+		System.out.println("大きいほうの値は" + (intValueA >intValueB ? intValueA : intValueB) + "です。");
+	}
+}

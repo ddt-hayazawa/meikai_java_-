ff48134600e07f3e177d134e3cf57c6cfe4d8cb1
hayazawa <ddt.khayazawa@gmail.com>
2019-12-25 19:49:18 +0900

    2019/12/25_演習03-14_1回目_進捗率100％_2つの実数値を読み込み小さい値・大きい値両方を表示する

--- Work/src/e_03_14/E_03_14.java
+++ Work/src/e_03_14/E_03_14.java
@@ -18,5 +18,20 @@ public class E_03_14 {
 		int intValueB = stdIn.nextInt();
 
 
+		// 比較用に代入値を最大値用の変数に代入
+		int maxValue = intValueA;
+		// 最小値も最小値用変数に代入
+		int minValue = intValueB;
+
+		if(maxValue < minValue){
+			// maxがminより小さい場合は入れ替えのため仮変数にbの値を代入
+			int tempVariable = maxValue;
+			// 実数値aの値を最大値用変数に代入
+			maxValue = minValue;
+			// 仮変数に入れておいた実数値bの値を最小値用変数に代入
+			minValue = tempVariable;
+		}
+		// 三項演算子で比較して表示
+		System.out.println((maxValue == minValue) ? "2つの値は同じです。" : "最大値は" + maxValue + "、最小値は" + minValue + "です。" );
 	}
 }

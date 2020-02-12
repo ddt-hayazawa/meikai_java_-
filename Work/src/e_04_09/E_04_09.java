ceb6454f8749b86b6683ebbd07379fccbe760bee
hayazawa <ddt.khayazawa@gmail.com>
2020-02-12 19:51:07 +0900

    2020/02/12_演習04-09_2回目_進捗率100％_正の整数値を読み込み1から入力値までの積を表示する

--- Work/src/e_04_09/E_04_09.java
+++ Work/src/e_04_09/E_04_09.java
@@ -11,23 +11,24 @@ public class E_04_09 {
 		// 入力値を保存する変数の宣言と代入
 		int inputNum = stdIn.nextInt();
 
+		// 積を求めるための変数を宣言し初期化
+		// 0を入れると掛け算ができないため1を代入
+		int sekiNum = 1;
+
 		// カウント用変数の宣言と代入
 		int i = 1;
-		// 積を入れるための変数を宣言し初期化
-		// 0を入れると掛け算ができないため1を代入
-		int multipicNum = 1;
 
 		while (inputNum > 0) {
-			// 入力値になるまで積用変数に1をかける
-			multipicNum *= 1;
+			// 入力値になるまで積用変数に入力値をかける
+			sekiNum *= i;
 			if (i == inputNum) {
 				// 入力値の回数までループしたら処理を抜ける
 				break;
 			}
 			// カウント
 			i++;
-			// 入力値を
-			System.out.println(inputNum);
+			// 積を求める変数を表示
 		}
+		System.out.println("1から" + inputNum + "までの積は" + sekiNum + "です。");
 	}
 }

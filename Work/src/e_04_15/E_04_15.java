b3068a7896ef242fa20f46fb66698a8f18ca875e
hayazawa <ddt.khayazawa@gmail.com>
2020-02-19 21:18:01 +0900

    2020/0②/19_演習04-15_2回目_進捗率100％_4-15を完了

--- Work/src/e_04_15/E_04_15.java
+++ Work/src/e_04_15/E_04_15.java
@@ -8,24 +8,42 @@ public class E_04_15 {
 		// Scannerインスタンスを生成
 		Scanner stdIn = new Scanner(System.in);
 
-		// 開始値の入力を促す
-		System.out.println("何cmから：");
-		// 入力値を変数に代入
-		int startNum = stdIn.nextInt();
+		// 開始値用変数の宣言
+		int startVal;
+		// 終了値用変数の宣言
+		int endVal;
+		// 増分値用変数の宣言
+		int displayVal;
 
-		// 終了値の入力を促す
-		System.out.println("何cmまで：");
-		// 入力値を変数に代入
-		int endNum = stdIn.nextInt();
+		do {
+			// 開始値の入力を促す
+			System.out.print("何cmから：");
+			// 入力値を変数に代入
+			startVal = stdIn.nextInt();
 
-		// 増分値の入力を促す
-		System.out.println("何cmごと：");
-		// 入力値を変数に代入
-		int diffNum = stdIn.nextInt();
+			// 終了値の入力を促す
+			System.out.print("何cmまで：");
+			// 入力値を変数に代入
+			endVal = stdIn.nextInt();
 
-		//
-		if(startNum <= 0 || endNum <= 0 || diffNum <= 0){
-			System.out.println("入力値が小さすぎます。");
+			// 増分値の入力を促す
+			System.out.print("何cmごと：");
+			// 入力値を変数に代入
+			displayVal = stdIn.nextInt();
+
+			// 身長が0cm以下だと整数ではなくなるため再入力を促す
+			if (startVal <= 0 || endVal <= 0 || displayVal <= 0) {
+				System.out.println("入力値が小さすぎます。");
+			}
+			// 身長が100cm以下だと計算できないため再入力を促す
+		} while (startVal <= 100 || endVal <= 100 || displayVal <= 0);
+
+		// 出力内容の説明
+		System.out.println("身長　標準体重");
+		// 終了値になるまで増分値をプラスする
+		for (int i = startVal; i <= endVal; i += displayVal) {
+			// displayValセンチごとに結果を出力する
+			System.out.println(i + " " + (i - 100) * 0.9);
 
 		}
 	}
